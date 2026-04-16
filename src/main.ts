f852f1ebbcecb9085fc9bf5f90d271d649f7d8da
José Albornoz <josealbornozsalazar@gmail.com>
2026-04-16 15:40:18 -0400

    update: add CORS to the frontend conection

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
